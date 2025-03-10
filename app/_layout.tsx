75221af686ee5272c511caed9f7902f06f5c97ee
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2025-03-10 11:50:51 +0800

    Add expo-dev-client and not-found

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
+import 'expo-dev-client';
+
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" options={{ title: 'Not Found' }} />
         </Stack>
     );
 }
